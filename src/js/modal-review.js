7120d58ad346b0e897d79e2a7384769dee07e5db
number-nine <sergii.pokhylko@gmail.com>
2023-03-18 00:35:17 +0200

    script rollback

--- src/js/modal-review.js
+++ src/js/modal-review.js
@@ -3,30 +3,30 @@
     openAddReviewBtn: document.querySelector('.top-sellers-button'),
     closeAddReviewBtn: document.querySelector('.modal-review__button-close'),
 
-    openTestModalBtn: document.querySelector('.top-sellers-img'),
-    closeTestModalBtn: document.querySelector('.test-modal'),
+    // openTestModalBtn: document.querySelector('.top-sellers-img'),
+    // closeTestModalBtn: document.querySelector('.test-modal'),
 
     backdrop: document.querySelector('.modal-review__backdrop'),
 
-    modal: document.querySelector('.modal-review'),
-    testModal: document.querySelector('.test-modal'),
+    addReviewModal: document.querySelector('.modal-review'),
+    // testModal: document.querySelector('.test-modal'),
     
     bodyNoScroll: document.querySelector('body'),
   };
 
-  refs.openAddReviewBtn.addEventListener ('click', () => toggleBackdrop(toggleAddReview));
-  refs.closeAddReviewBtn.addEventListener('click', () => toggleBackdrop(toggleAddReview));
+  refs.openAddReviewBtn.addEventListener ('click', () => toggleBackdrop(toggleAddReviewModal));
+  refs.closeAddReviewBtn.addEventListener('click', () => toggleBackdrop(toggleAddReviewModal));
 
-  refs.openTestModalBtn.addEventListener ('click', () => toggleBackdrop(toggleTestModal));
-  refs.closeTestModalBtn.addEventListener('click', () => toggleBackdrop(toggleTestModal));
+  // refs.openTestModalBtn.addEventListener ('click', () => toggleBackdrop(toggleTestModal));
+  // refs.closeTestModalBtn.addEventListener('click', () => toggleBackdrop(toggleTestModal));
 
-  function toggleAddReview() {
-    refs.modal.classList.toggle('modal-review--hidden');
+  function toggleAddReviewModal() {
+    refs.addReviewModal.classList.toggle('modal-review--hidden');
   }
 
-  function toggleTestModal() {
-    refs.testModal.classList.toggle('test-modal--hidden');
-  }
+  // function toggleTestModal() {
+  //   refs.testModal.classList.toggle('test-modal--hidden');
+  // }
 
   function toggleBackdrop(callback) {
     refs.backdrop.classList.toggle('modal-review__backdrop--hidden');
