5a63f3eeb1a2f8468e365a56d44c32a9e4f53187
number-nine <sergii.pokhylko@gmail.com>
2023-03-18 18:29:55 +0200

    modal+popup+script

--- src/js/modal-review.js
+++ src/js/modal-review.js
@@ -1,38 +1,60 @@
 (() => {
   const refs = {
-    openAddReviewBtn: document.querySelector('.top-sellers-button'),
-    closeAddReviewBtn: document.querySelector('.modal-review__button-close'),
 
-    // openTestModalBtn: document.querySelector('.top-sellers-img'),
-    // closeTestModalBtn: document.querySelector('.test-modal'),
-
+    selectors: [
+      {
+      openBtn: '.top-sellers-button',
+      closeBtn: '.js-reviewClose',
+      submitBtn: '.js-reviewSubmit',
+      closePopupBtn: '.js-reviewPopupClose',
+      modalRootClass: '.modal-review',
+      modalHideClass: 'modal-review--hidden',
+      popupRootClass: '.popup-review',
+      popupHideClass: 'popup-review--hidden',
+      },
+    ],
     backdrop: document.querySelector('.modal-review__backdrop'),
-
-    addReviewModal: document.querySelector('.modal-review'),
-    // testModal: document.querySelector('.test-modal'),
-    
     bodyNoScroll: document.querySelector('body'),
+
+    init: function (selectors) {
+      for (let paramSet of selectors){
+        document
+        .querySelector(paramSet.openBtn)
+        .addEventListener('click', () => toggleModal(paramSet.modalRootClass, paramSet.modalHideClass) );
+        
+        document
+        .querySelector(paramSet.closeBtn)
+        .addEventListener('click', () => toggleModal(paramSet.modalRootClass, paramSet.modalHideClass) );
+
+        document
+        .querySelector(paramSet.submitBtn)
+        .addEventListener('click', () => showPopup(paramSet.modalRootClass, paramSet.modalHideClass, paramSet.popupRootClass, paramSet.popupHideClass) );
+
+        document
+        .querySelector(paramSet.closePopupBtn)
+        .addEventListener('click', () => toggleModal(paramSet.popupRootClass, paramSet.popupHideClass) );
+      }
+    },
+     
   };
 
-  refs.openAddReviewBtn.addEventListener ('click', () => toggleBackdrop(toggleAddReviewModal));
-  refs.closeAddReviewBtn.addEventListener('click', () => toggleBackdrop(toggleAddReviewModal));
-
-  // refs.openTestModalBtn.addEventListener ('click', () => toggleBackdrop(toggleTestModal));
-  // refs.closeTestModalBtn.addEventListener('click', () => toggleBackdrop(toggleTestModal));
-
-  function toggleAddReviewModal() {
-    refs.addReviewModal.classList.toggle('modal-review--hidden');
-  }
-
-  // function toggleTestModal() {
-  //   refs.testModal.classList.toggle('test-modal--hidden');
-  // }
-
-  function toggleBackdrop(callback) {
+  function toggleBackdrop() {
     refs.backdrop.classList.toggle('modal-review__backdrop--hidden');
     refs.bodyNoScroll.classList.toggle('page__body--no-scroll');
-    callback();
   }
 
+  function toggleModal(rootClass, hideClass) {
+    console.log(rootClass);
+    console.log(hideClass);
+    document.querySelector(rootClass).classList.toggle(hideClass);
+    toggleBackdrop();
+  }
+
+  function showPopup(modalRootClass, modalHideClass, popupRootClass, popupHideClass) {
+    document.querySelector(modalRootClass).classList.toggle(modalHideClass);
+    document.querySelector(popupRootClass).classList.toggle(popupHideClass);
+  }
+
+  refs.init(refs.selectors);
 
 })();
