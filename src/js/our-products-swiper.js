35b9873e223c4ec698a67e83867cfdd5cdbbd360
OleksandrStasiuk <122239424+OleksandrStasiuk@users.noreply.github.com>
2023-03-20 01:02:56 +0200

    Final fixes

--- src/js/our-products-swiper.js
+++ src/js/our-products-swiper.js
@@ -1,6 +1,12 @@
 const swiper = new Swiper('.swiper', {
   spaceBetween: 18,
   slidesPerView: 1,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  mousewheel: {
+    sensitivity: 1,
+  },
+  loop: true,
   breakpoints: {
     768: {
       slidesPerView: 2.5,
