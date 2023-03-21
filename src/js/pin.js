236a28bcd4cc4d18ee40046d16ecc0255d2b4461
number-nine <sergii.pokhylko@gmail.com>
2023-03-21 23:01:06 +0200

    stiky

---
+++ src/js/pin.js
@@ -0,0 +1,7 @@
+const el = document.querySelector(".header")
+const observer = new IntersectionObserver( 
+  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+  { threshold: [1] }
+);
+
+observer.observe(el);
